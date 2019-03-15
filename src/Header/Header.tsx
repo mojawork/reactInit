26f640c384f93c876515e10c81bb69f2af11758c
Markus Lungwitz <webmaster@moja-malu.de>
2019-03-15 22:45:39 +0100

    first commit

---
+++ src/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.scoped.scss';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                Header
+            </header>
+        );
+    }
+}
+
+export default Header;
---
+++ src/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Main.scoped.scss';
+
+class Main extends Component {
+    render() {
+        return (
+            <main>
+                Main
+            </main>
+        );
+    }
+}
+
+export default Main;
